4f355ad5a2852e713d0dd07c679306d42d072f97
KellyCarvalho <18kellycarvalho@gmail.com>
2021-07-14 21:06:29 -0300

    backend porject created

---
+++ backend/swcatalog/src/main/java/com/saleswarning/swcatalog/SwcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.saleswarning.swcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwcatalogApplication.class, args);
+	}
+
+}

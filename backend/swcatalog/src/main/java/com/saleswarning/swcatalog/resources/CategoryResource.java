3247f203f85fce36c9401a6dff1a46d10287ca06
KellyCarvalho <18kellycarvalho@gmail.com>
2021-07-16 21:37:56 -0300

    Request findById e tratamento de exceções

--- backend/swcatalog/src/main/java/com/saleswarning/swcatalog/resources/CategoryResource.java
+++ backend/swcatalog/src/main/java/com/saleswarning/swcatalog/resources/CategoryResource.java
@@ -6,11 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saleswarning.swcatalog.dto.CategoryDTO;
-import com.saleswarning.swcatalog.entities.Category;
 import com.saleswarning.swcatalog.services.CategoryService;
 @RestController
 @RequestMapping(value="/categories")
@@ -27,6 +27,13 @@ public class CategoryResource implements Serializable{
 		
 	}
 	
+	@GetMapping(value ="/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok().body(dto);
+		
+	}
 	
 
 }
--- backend/swcatalog/src/main/java/com/saleswarning/swcatalog/services/CategoryService.java
+++ backend/swcatalog/src/main/java/com/saleswarning/swcatalog/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.saleswarning.swcatalog.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.saleswarning.swcatalog.dto.CategoryDTO;
 import com.saleswarning.swcatalog.entities.Category;
 import com.saleswarning.swcatalog.repositories.CategoryRepository;
+import com.saleswarning.swcatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -34,5 +36,16 @@ public class CategoryService {
 		
 		return listDTO;
 	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		//optional evitar que o ID seja nulo
+		Optional<Category> obj =repository.findById(id);
+		Category entity = obj.orElseThrow(()-> new EntityNotFoundException("Category not found"));
+		
+		return new CategoryDTO(entity) ;
+	}
+	
+	
 
 }
